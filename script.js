1ca5b8357a663c374c69f3f94038c5d096c99295
Pranshav Patel <pranshavpatel1803@gmail.com>
2022-09-26 19:51:59 +0530

    added the final changes
    Completed the entire project might light/dark mode toggle later

--- script.js
+++ script.js
@@ -8,25 +8,54 @@ const givenMeters = document.querySelector("#given-meter")
 const ansFeet = document.querySelector("#ans-feet")
 const givenFeet = document.querySelector("#given-feet")
 const ansMeters = document.querySelector("#ans-meters")
+
+const givenLitres = document.querySelector("#given-litres")
+const ansLitres = document.querySelector("#ans-litres")
+const givenGallons = document.querySelector("#given-gallons")
+const ansGallons = document.querySelector("#ans-gallons")
+
+const givenKilograms = document.querySelector("#given-kilograms")
+const ansKilograms = document.querySelector("#ans-kilograms")
+const givenPounds = document.querySelector("#given-pounds")
+const ansPounds = document.querySelector("#ans-pounds")
+
 const convertBtn = document.querySelector("#convert-btn")
+const inputField = document.querySelector("#ans")
 
 
 
 convertBtn.addEventListener('click', () => {
-    const inputVal = document.querySelector("#ans").value
+    const inputVal = inputField.value
     if(inputVal){
-        // Section Length
+        // Length Section
         let aMeters = inputVal * 0.304
         let aFeet = inputVal * 3.281
-
-        parseFloat(aMeters).toFixed(3)
-        parseFloat(aFeet).toFixed(3)
 
         givenMeters.textContent = inputVal
         givenFeet.textContent = inputVal
 
-        ansFeet.textContent = aFeet
-        ansMeters.textContent = aMeters
+        ansFeet.textContent = aFeet.toFixed(3)
+        ansMeters.textContent = aMeters.toFixed(3)
+
+        // Volume Section
+        let aLitres = inputVal * 3.785
+        let aGallons = inputVal * 0.264
+
+        givenLitres.textContent = inputVal
+        givenGallons.textContent = inputVal
+
+        ansLitres.textContent = aLitres.toFixed(3)
+        ansGallons.textContent = aGallons.toFixed(3)
+
+        // Mass Section
+        let aKilograms = inputVal * 0.453
+        let aPounds = inputVal * 2.204
+
+        givenKilograms.textContent = inputVal
+        givenPounds.textContent = inputVal
+
+        ansKilograms.textContent = aKilograms.toFixed(3)
+        ansPounds.textContent = aPounds.toFixed(3)
     }
     
 
