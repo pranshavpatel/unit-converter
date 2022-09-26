92db9a0c7e0af62bf256acf7b9aa28b0509c41db
Pranshav Patel <pranshavpatel1803@gmail.com>
2022-09-26 11:53:32 +0530

    initial js code
    Added the code for length unit conversion

--- script.js
+++ script.js
@@ -3,3 +3,31 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const givenMeters = document.querySelector("#given-meter")
+const ansFeet = document.querySelector("#ans-feet")
+const givenFeet = document.querySelector("#given-feet")
+const ansMeters = document.querySelector("#ans-meters")
+const convertBtn = document.querySelector("#convert-btn")
+
+
+
+convertBtn.addEventListener('click', () => {
+    const inputVal = document.querySelector("#ans").value
+    if(inputVal){
+        // Section Length
+        let aMeters = inputVal * 0.304
+        let aFeet = inputVal * 3.281
+
+        parseFloat(aMeters).toFixed(3)
+        parseFloat(aFeet).toFixed(3)
+
+        givenMeters.textContent = inputVal
+        givenFeet.textContent = inputVal
+
+        ansFeet.textContent = aFeet
+        ansMeters.textContent = aMeters
+    }
+    
+
+})
